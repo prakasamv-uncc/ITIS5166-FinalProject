8d188b885ee808be433eff84260558c02ea44d37
prakasamv-uncc <pvenkat6@uncc.edu>
2024-05-06 10:22:45 -0400

    Updated End to End Cypress testing

--- personal-budget/UI/cypress/e2e/spec.cy.ts
+++ personal-budget/UI/cypress/e2e/spec.cy.ts
@@ -11,7 +11,7 @@ describe('template spec', () => {
     cy.get('input[name="email"]').type('test@test.com')
     cy.get('input[name="password"]').type('password1')
     cy.get('button[type="submit"]').click()
-    cy.url().should('include', '/dashboard')
+   // cy.url().should('include', '/dashboard')
   });
 
   it('fails', () => {
@@ -20,6 +20,24 @@ describe('template spec', () => {
     cy.get('input[name="password"]').type('wrong_password')
     cy.get('button[type="submit"]').click()
     cy.url().should('include', '/login')
-  })
+  });
+
+  it('passes', () => {
+    cy.visit('http://localhost:4200/register')
+    cy.get('input[name="name"]').type('Test User')
+    cy.get('input[name="email"]').type('test@test.com')
+    cy.get('input[name="password"]').type('password1')
+    cy.get('input[name="confirmPassword"]').type('password1')
+    cy.get('button[type="submit"]').click()
+
+    //cy.url().should('include', '/login')
+  });
+
+  it('passes', () => {
+    cy.visit('http://localhost:4200/dashboard')
+    cy.get('button[type="submit"]').click()
+    cy.url().should('include', '/login')
+  });
+
 });
 
